1aa3279153ca936c4594522cbe9cdd7e1b02e7e5
Gus <gus.fouad@gmail.com>
2020-06-20 04:37:46 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -3,24 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Landing Page</h1>
+			<h2>
+				<a href="http://localhost:3001/cloudflix">Cloudflix</a>
+			</h2>
+		</div>
+	);
 }
 
 export default App;
